2b51becbad34ca53ea4d625a00123be8c1775229
Artem Musienko <temich2001@yandex.ru>
2023-09-16 22:00:39 +0300

    A pagination system and a personal user list with him blogs.

--- main/blog/urls.py
+++ main/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import PostlistView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostlistView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostlistView
 
 urlpatterns = [
     path('', PostlistView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostlistView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
